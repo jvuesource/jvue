ebfc744af684645c74a51f2f819a98a381f41564
Terwer <youweics@sina.com>
2019-02-17 00:22:20 +0800

    Init v1.4.0

--- src/main/java/com/terwergreen/jvue/JVueApplication.java
+++ src/main/java/com/terwergreen/jvue/JVueApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JVueApplication {
+public class JvueApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(JVueApplication.class, args);
+        SpringApplication.run(JvueApplication.class, args);
     }
+
 }
 
