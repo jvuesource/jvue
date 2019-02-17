5b7b4d052975f8f68eeefeb381cbe06803573fd3
Terwer <youweics@sina.com>
2019-02-17 17:24:17 +0800

    Try to export v8-server

--- src/main/vue/server/v8-server.js
+++ src/main/vue/server/v8-server.js
@@ -3,36 +3,26 @@ const { Nuxt, Builder } = require("nuxt");
 const config = require("../nuxt.config.js");
 config.dev = !(process.env.NODE_ENV === "production");
 
-const nuxt = new Nuxt(config);
-
 // ==================
 // test Server
 // ==================
 
-const context = {};
+async function init() {
+  const nuxt = new Nuxt(config);
 
-if (config.dev) {
-  console.log("build server");
-  new Builder(nuxt).build().then(() => {
-    renderServer(context);
-  });
-} else {
-  renderServer(context);
+  console.log("building");
+
+  await new Builder(nuxt).build();
+
+  console.log("built");
+
+  await nuxt.listen(4000, "localhost");
+
+  console.log("listening...");
+
+  const res = await nuxt.renderRoute("/");
+
+  console.log(res);
 }
 
-function renderServer(context) {
-  console.log("context=>", context);
-
-  nuxt.renderRoute("/").then(({ html, error, redirected }) => {
-    // `html` will be always a string
-    console.log("html=>", html);
-
-    // `error` not null when the error layout is displayed, the error format is:
-    // { statusCode: 500, message: 'My error message' }
-    console.log("error=>", error);
-
-    // `redirected` is not `false` when `redirect()` has been used in `asyncData()` or `fetch()`
-    // { path: '/other-path', query: {}, status: 302 }
-    console.log("redirected=>", redirected);
-  });
-}
+init();
