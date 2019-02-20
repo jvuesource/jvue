6d4becbdeb416d9ce6c4356cfc2b7a8604285734
terwer <CBGtyw2050*#>
2019-02-20 13:26:05 +0800

    Unhandled Rejection with Spring Boot

--- src/main/vue/src/entry-client.js
+++ src/main/vue/src/entry-client.js
@@ -12,42 +12,6 @@ if (window.__INITIAL_STATE__) {
 // wait until router has resolved all async before hooks
 // and async components...
 router.onReady(() => {
-  const matchedComponents = router.getMatchedComponents();
-  // 匹配不到的路由，执行reject函数，并返回 404
-  if (!matchedComponents.length) {
-    console.error("404 Not Found");
-    return;
-  }
-  console.info("matchedComponents=>", matchedComponents[0].name);
-
-  // 对所有匹配的路由组件调用 `asyncData()`
-  Promise.all(
-    matchedComponents.map(Component => {
-      console.log("Client matched Component");
-      if (Component.asyncData) {
-        console.log("调用Component.asyncData获取数据");
-        return Component.asyncData({
-          store
-        });
-      } else {
-        console.log("未找到asyncData");
-      }
-    })
-  )
-    .then(() => {
-      // 在所有预取钩子(preFetch hook) resolve 后，
-      // 我们的 store 现在已经填充入渲染应用程序所需的状态。
-      // 当我们将状态附加到上下文，
-      // 并且 `template` 选项用于 renderer 时，
-      // 状态将自动序列化为 `window.__INITIAL_STATE__`，并注入 HTML。
-      window.__INITIAL_STATE__ = store.state;
-      console.log("store.state=>", store.state);
-      console.log("window.__INITIAL_STATE__=>", window.__INITIAL_STATE__);
-    })
-    .catch(() => {
-      console.log("init asyncData invoke error");
-    });
-
   // Add router hook for handling asyncData.
   // Doing it after initial route is resolved so that we don't double-fetch
   // the data that we already have. Using router.beforeResolve() so that all
--- src/main/vue/src/entry-server.js
+++ src/main/vue/src/entry-server.js
@@ -1,11 +1,14 @@
 import { createApp } from "./app";
 
 // This exported function will be called by `bundleRenderer`.
+// This is where we perform data-prefetching to determine the
+// state of our application before actually rendering it.
+// Since data fetching is async, this function is expected to
 // return a Promise that resolves to the app instance.
 export default context => {
   return new Promise((resolve, reject) => {
     console.log("entry server received context=>", context);
-    const { app, router } = createApp(context);
+    const { app, router, store } = createApp(context);
 
     router.push(context.url);
 
@@ -13,15 +16,42 @@ export default context => {
     router.onReady(() => {
       const matchedComponents = router.getMatchedComponents();
       // 匹配不到的路由，执行reject函数，并返回 404
+      // 匹配不到的路由，执行reject函数，并返回 404
       if (!matchedComponents.length) {
-        return reject({
-          status: 0,
-          data: "No matchedComponents",
-          msg: "404 Not Found"
-        });
+        console.error("404 Not Found");
+        return reject;
       }
 
-      resolve(app);
+      // Call fetchData hooks on components matched by the route.
+      // A preFetch hook dispatches a store action and returns a Promise,
+      // which is resolved when the action is complete and store state has been
+      // updated.
+      // 对所有匹配的路由组件调用 `asyncData()`
+      Promise.all(
+        matchedComponents.map(Component => {
+          console.log("Server matched Component");
+          if (Component.asyncData) {
+            console.log("Server 调用Component.asyncData获取数据");
+            return Component.asyncData({
+              store
+            });
+          } else {
+            console.log("Server 未找到asyncData");
+          }
+        })
+      )
+        .then(() => {
+          // 在所有预取钩子(preFetch hook) resolve 后，
+          // 我们的 store 现在已经填充入渲染应用程序所需的状态。
+          // 当我们将状态附加到上下文，
+          // 并且 `template` 选项用于 renderer 时，
+          // 状态将自动序列化为 `window.__INITIAL_STATE__`，并注入 HTML。
+          context.state = store.state;
+          // Promise应该resolve app
+          console.log("Promise resolved success");
+          resolve(app);
+        })
+        .catch(reject);
     }, reject);
   });
 };
