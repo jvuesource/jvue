9398fa3f2df4a2c133b0099019b76d6084a593e4
Terwer <youweics@sina.com>
2019-02-27 12:52:34 +0800

    Add entry-client build

---
+++ src/main/vue/src/server.js
@@ -0,0 +1,26 @@
+/**
+ * server.js
+ *
+ *@author Terwer
+ *@version 1.0
+ *2019/2/27 12:05
+ **/
+// const createApp = require("/path/to/built-server-bundle.js");
+//
+// server.get("*", (req, res) => {
+//   const context = { url: req.url };
+//
+//   createApp(context).then(app => {
+//     renderer.renderToString(app, (err, html) => {
+//       if (err) {
+//         if (err.code === 404) {
+//           res.status(404).end("Page not found");
+//         } else {
+//           res.status(500).end("Internal Server Error");
+//         }
+//       } else {
+//         res.end(html);
+//       }
+//     });
+//   });
+// });
--- src/main/vue/vue.config.js
+++ src/main/vue/vue.config.js
@@ -6,6 +6,8 @@
  * 19-2-26 下午10:40
  **/
 const pkg = require("./package.json");
+const VueSSRClientPlugin = require("vue-server-renderer/client-plugin");
+
 module.exports = {
   /**
    * 部署应用包时的基本 URL。用法和 webpack 本身的 output.publicPath 一致，
@@ -20,6 +22,42 @@ module.exports = {
   lintOnSave: true,
   // https://cli.vuejs.org/zh/config/#configurewebpack
   configureWebpack: config => {
+    console.log("process.env.SSR_ENV=>", process.env.SSR_ENV);
+    switch (process.env.SSR_ENV) {
+      case "client":
+        {
+          // 客户端配置 (Client Config)
+          // https://ssr.vuejs.org/zh/guide/build-config.html#客户端配置-client-config
+          console.log("客户端配置");
+
+          // 重要信息：这将 webpack 运行时分离到一个引导 chunk 中，
+          // 以便可以在之后正确注入异步 chunk。
+          // 这也为你的 应用程序/vendor 代码提供了更好的缓存。
+          //webpack4的用法：https://github.com/webpack/webpack/issues/6357
+          config.optimization = {
+            splitChunks: {
+              chunks: "all"
+            }
+          };
+
+          // 此插件在输出目录中
+          // 生成 `vue-ssr-client-manifest.json`。
+          config.plugins.push(new VueSSRClientPlugin());
+        }
+        break;
+      case "server": {
+        // 服务器配置 (Server Config)
+        // https://ssr.vuejs.org/zh/guide/build-config.html#服务器配置-server-config
+        console.log("服务器配置");
+        break;
+      }
+      default: {
+        // serve模式
+        console.log("serve模式");
+        break;
+      }
+    }
+
     if (process.env.NODE_ENV === "production") {
       // 为生产环境修改配置...
     } else {
@@ -28,6 +66,7 @@ module.exports = {
   },
   // https://cli.vuejs.org/zh/config/#chainwebpack
   chainWebpack: config => {
+    // 配置html-webpack-plugin
     config.plugin("html").tap(args => {
       // inject为true会自动在html文件中添加js和css引用
       args[0].inject = true;
